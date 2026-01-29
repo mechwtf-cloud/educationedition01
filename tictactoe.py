d88bd957d1b9a04f1f6a2e4f57ab603fda488789
mechwtf-cloud <mech.wtf@gmail.com>
2026-01-29 17:39:43 +0100

    minimax added kind of

--- tictactoe.py
+++ tictactoe.py
@@ -17,30 +17,31 @@ coords = [
     [(start_x+1, start_y, start_z),   (start_x+1, start_y, start_z+1),   (start_x+1, start_y, start_z+2)],
     [(start_x+2, start_y, start_z),   (start_x+2, start_y, start_z+1),   (start_x+2, start_y, start_z+2)]
 ]
+
 def start_game():
-    
     for i in range(3):
         for j in range(3):
             x, y, z = coords[i][j]
             blocks.place(EMPTY, world(x, y, z))
-
-    player.say("doska hotova")
-    
-
+    player.say("Doska hotova")
 
 player.on_chat("start", start_game)
 
 def player_move_cmd(num):
+    if num < 0 or num > 8:
+        player.say("Enter 0-8")
+        return
+
     i = num // 3
     j = num % 3
 
     if board[i][j] != 0:
-        player.say("obsadene ")
+        player.say("Obsadene")
         return
 
     board[i][j] = 1
     x, y, z = coords[i][j]
-    blocks.place(RED_CONCRETE, world(x, y, z))
+    blocks.place(PLAYER, world(x, y, z))
 
     bot_move()
 
@@ -50,7 +51,7 @@ def bot_move():
             if board[i][j] == 0:
                 board[i][j] = 2
                 x, y, z = coords[i][j]
-                blocks.place(BLUE_CONCRETE, world(x, y, z))
+                blocks.place(BOT, world(x, y, z))
                 return
 
 player.on_chat("move", player_move_cmd)
