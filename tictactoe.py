71357ad3f06d3065293d90a51a52defd3f492144
mechwtf-cloud <mech.wtf@gmail.com>
2026-01-29 16:10:53 +0100

    Refactor Tic Tac Toe game initialization and moves

--- tictactoe.py
+++ tictactoe.py
@@ -2,9 +2,9 @@ EMPTY = WHITE_CONCRETE
 PLAYER = RED_CONCRETE
 BOT = BLUE_CONCRETE
 
-start_x = 10
+start_x = 0
 start_y = 5
-start_z = 10
+start_z = 0
 
 board = [
     [0,0,0],
@@ -13,36 +13,44 @@ board = [
 ]
 
 coords = [
-    [(start_x,start_y,start_z),(start_x,start_y,start_z+1),(start_x,start_y,start_z+2)],
-    [(start_x+1,start_y,start_z),(start_x+1,start_y,start_z+1),(start_x+1,start_y,start_z+2)],
-    [(start_x+2,start_y,start_z),(start_x+2,start_y,start_z+1),(start_x+2,start_y,start_z+2)]
+    [(start_x, start_y, start_z),     (start_x, start_y, start_z+1),     (start_x, start_y, start_z+2)],
+    [(start_x+1, start_y, start_z),   (start_x+1, start_y, start_z+1),   (start_x+1, start_y, start_z+2)],
+    [(start_x+2, start_y, start_z),   (start_x+2, start_y, start_z+1),   (start_x+2, start_y, start_z+2)]
 ]
+def start_game():
+    
+    for i in range(3):
+        for j in range(3):
+            x, y, z = coords[i][j]
+            blocks.place(EMPTY, world(x, y, z))
 
-for i in range(3):
-    for j in range(3):
-        x,y,z = coords[i][j]
-        blocks.place(EMPTY, world(x,y,z))
+    player.say("doska hotova")
+    
 
-player.say("DOSKA OK ")
 
-player_move = 4  
+player.on_chat("start", start_game)
 
-i = player_move // 3
-j = player_move % 3
+def player_move_cmd(num):
+    i = num // 3
+    j = num % 3
 
-if board[i][j] == 0:
+    if board[i][j] != 0:
+        player.say("obsadene ")
+        return
+
     board[i][j] = 1
-    x,y,z = coords[i][j]
-    blocks.place(RED_CONCRETE, world(x,y,z))
+    x, y, z = coords[i][j]
+    blocks.place(RED_CONCRETE, world(x, y, z))
 
-# bot odpovie – prva volna bunka
-for bi in range(3):
-    for bj in range(3):
-        if board[bi][bj] == 0:
-            board[bi][bj] = 2
-            x,y,z = coords[bi][bj]
-            blocks.place(BLUE_CONCRETE, world(x,y,z))
-            break
-    else:
-        continue
-    break
+    bot_move()
+
+def bot_move():
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 0:
+                board[i][j] = 2
+                x, y, z = coords[i][j]
+                blocks.place(BLUE_CONCRETE, world(x, y, z))
+                return
+
+player.on_chat("move", player_move_cmd)
